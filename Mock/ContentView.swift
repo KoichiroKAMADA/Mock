d190a53fe7d6e73f99af8d993efcb79c2111d004
KoichiroKAMADA <516k@516k.net>
2022-09-24 21:36:49 +0900

    Initial Commit

---
+++ Mock/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Mock
+//
+//  Created by KoichiroKAMADA on 2022/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Mock/MockApp.swift
@@ -0,0 +1,17 @@
+//
+//  MockApp.swift
+//  Mock
+//
+//  Created by KoichiroKAMADA on 2022/09/24.
+//
+
+import SwiftUI
+
+@main
+struct MockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
